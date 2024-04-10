c63c39779e398d93adade758338c4756b78438ce
Vishal K Sahu <hello@vishalkumarsahu.in>
2024-04-10 22:06:28 +0530

    v0.1: Coverts PDF to different image formats (emf, wmf, jpg, etc)

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# bimbman
-Image converter and resizer for research publication.
+# **BIMBMAAN**: A Shaktiman to process high quality images for research publication
+
+BIMBMAAN is a powerful tool designed to assist researchers in dealing with image resizing and conversion challenges encountered during manuscript preparation for research publication. Its primary goal is to simplify the process of handling high-quality images, ensuring they meet the standards required for publication.
+
+# Details and Descriptions
+
+## Features
+* Efficient image resizing to desired dimensions (e.g., 600 DPI, 1200 DPI).
+* Seamless conversion between different image formats (e.g., PNG, JPEG, TIFF).
+* Batch processing capabilities for handling multiple images simultaneously.
+* Preservation of image quality and resolution to meet publication requirements.
+* Easy to install and operate and works with single commands without requiring any customisation
+
+## How to Use
+* Install BIMBMAAN using `pip install bimbmaan`.
+* Navigate to your images directory where your images are ready to process.
+* Use command `bimbkar` or `bimbman` for batch processing.
+
+## Benefits
+* Saves time and effort by automating image processing tasks.
+* Ensures compliance with publication guidelines regarding image quality and resolution.
+* Reduces the risk of errors and inconsistencies in image conversion and dimensions of the images.
+
+# Feedback and Support
+We welcome any feedback or suggestions for improving BIMBMAAN. Please report issues through 
+[GitHub issues](https://github.com/TheBiomics/bimbmaan/issues).
---
+++ bimbmaan/manager.py
@@ -0,0 +1,73 @@
+from UtilityLib import ProjectManager
+import fitz as PyFPDF
+from PIL import Image
+
+class BimbManager(ProjectManager):
+  program = "BimbMaan"
+  build = 20240410
+
+  def __init__(self, *args, **kwargs):
+    super().__init__(**kwargs)
+
+  def process_args(self):
+    # key: (['arg_k1', 'arg_k2'], nargs, default, help, {})
+    _version_info = f"{self.program} (build-{self.build})"
+    _cli_settings = {
+      "path_base": (['-b'], "*", self.OS.getcwd(), 'Provide base directory to run the process.', {}),
+      "path_file": (['-f'], "*", None, 'Provide file path to process.', {}),
+      "image_width": (['-iw'], "*", 11.7, 'Output image width in inches.', {}),
+      "image_height": (['-ih'], "*", 8.3, 'Output image height in inches.', {}),
+      "dpi": (['-d'], "*", [72, 600], 'Output image DPI.', {}),
+      "extensions": (['-e'], "*", ["jpg"], 'Output image DPI for output quality. Most of the image extensions are allowed are recognised.', {}),
+    }
+
+    _params = self.get_cli_args(_cli_settings, version=_version_info)
+
+    if hasattr(self, 'setattrs'):
+      self.setattrs(_params)
+    else:
+      self.update_attributes(self, _params)
+
+  def _pdf_to_img(self, _pdf_path):
+    _i_width = getattr(self, 'image_width')
+    _i_height = getattr(self, 'image_height')
+
+    _dpis = getattr(self, 'dpi')
+    _exts = getattr(self, 'extensions')
+
+    _write_file_path = _pdf_path
+
+    _pdf_pages = PyFPDF.open(_pdf_path)
+
+    _allowed_extensions = Image.registered_extensions()
+    
+    for _pi, page in self.PB(enumerate(_pdf_pages)):
+      _pix = page.get_pixmap()
+      _pix = PyFPDF.Pixmap(_pix, 0)
+
+      _im = Image.frombytes("RGB", [_pix.width, _pix.height], _pix.samples)
+
+      for _dpi in _dpis:
+        _dpi = float(_dpi)
+        _im.info['dpi'] = (_dpi, _dpi)
+        _image_dimension = int(_i_width*_dpi), int(_i_height*_dpi)
+        _im = _im.resize(_image_dimension, Image.LANCZOS)
+        for _ext in _exts:
+          if not f".{_ext.lower()}" in _allowed_extensions:
+            continue
+          _type = _allowed_extensions.get(f".{_ext.lower()}")
+          # Slide number using PDF file name when there are multiple PDF files 
+          # Or create a directory with the PDF file name and generate images there
+          _img_path = self.change_ext(_write_file_path, f"S{_pi+1}.dpi{_dpi:.0f}.{_ext}")
+          _im.save(_img_path, _type, quality=100, dpi=(_dpi, _dpi))
+
+  def magic(self):
+    self.process_args()
+    print("Starting image conversion...")
+    if getattr(self, 'path_file'):
+      self._pdf_to_img(getattr(self, 'path_file'))
+    else:
+      _pdfs = self.search_files(self.get_path(), "*.pdf")
+      for _pdf in _pdfs:
+        self._pdf_to_img(_pdf)
+    print("Image conversion completed...")
